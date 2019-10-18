2fafbb13d4a4b582f113c37024580ba22b8676fa
kendrang <kendrang@users.noreply.github.com>
2019-10-18 18:57:14 -0400

    finsihed ifs half of loops

--- WriteIFs.java
+++ WriteIFs.java
@@ -9,6 +9,8 @@ public class WriteIFs
 {
  
     public void playerDied(boolean player1) {
+        if (isAlive(player1) == false){
+        displayGameOver(player1);}
         // Write an IF statement that checks “player1.isAlive()” 
         // and if that’s false, calls “displayGameOver(player1)”
      
@@ -18,6 +20,10 @@ public class WriteIFs
         // Write an IF statement that checks the 
         // “temperature(room)” and if that check is less than 70, 
         // calls “heatOn()” else calls “coolOn()”
+        if(temperature(room)<70){
+        heatOn();
+        }
+        else {coolOn();}
 
 
         
@@ -25,7 +31,11 @@ public class WriteIFs
     }
 
     public void fireplaceControl(Object fireplace1) {
+        
+        if (outsideTemp()<50 && insideTemp()<62){
+        startAFire(fireplace1);}
         // Write an IF statement that checks 
+     
         // “outsideTemp()” is less than 50 
         // AND 
         // “insideTemp()” is less than 62, 
@@ -34,6 +44,8 @@ public class WriteIFs
     }
 
     public void checkFuel(double fuelLevel) {
+        if(fuelLevel< 0.08){
+        refuel();}
         // Write an IF statement that checks “fuelLevel” 
         // and if that check is less than 0.08, calls “refuel()”
 
@@ -46,7 +58,7 @@ public class WriteIFs
      * 
      * 
      * instance variables
-     * / 
+     */ 
    int x;
    int tt_t;
    int tt_s;
@@ -72,7 +84,7 @@ public class WriteIFs
     public boolean isAlive(boolean p) {
         return !p;
     }
-    private int tempurature(int t) {
+    private int temperature(int t) {
         return t+2;
     }
     private void heatOn() {
