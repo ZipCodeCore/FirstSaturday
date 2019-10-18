8e95180984fa892f15fee476fc5f49a451aa8792
SandyPham823 <sandydupham@gmail.com>
2019-10-18 17:58:40 -0400

    Finished Lab

--- WriteIFs.java
+++ WriteIFs.java
@@ -2,25 +2,30 @@
 /**
  * Write a description of class WriteIFs here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Sandy Pham)
+ * @version (10/18/19)
  */
 public class WriteIFs
 {
- 
+
     public void playerDied(boolean player1) {
-        // Write an IF statement that checks “player1.isAlive()” 
+        // Write an IF statement that checks “player1.isAlive()”
+        if (player1 == true){
+            displayGameOver(player1);
+        }
         // and if that’s false, calls “displayGameOver(player1)”
-     
     }
-    
+
     public String thermoSTAT(int room) {
-        // Write an IF statement that checks the 
-        // “temperature(room)” and if that check is less than 70, 
-        // calls “heatOn()” else calls “coolOn()”
+        // Write an IF statement that checks the
+        if (room < 70){
+            heatOn();
+        }else{
+            coolOn();
+            // “temperature(room)” and if that check is less than 70, 
+            // calls “heatOn()” else calls “coolOn()”
 
-
-        
+        }
         return this.ss;
     }
 
@@ -29,71 +34,90 @@ public class WriteIFs
         // “outsideTemp()” is less than 50 
         // AND 
         // “insideTemp()” is less than 62, 
-        // calls “startAFire(fireplace1)”
+        if (outsideTemp() < 50 && insideTemp() < 62)
+            startAFire(fireplace1);
+        if (outsideTemp() < 50 && insideTemp() == 65)
+            startAFire(fireplace1);
+        if (outsideTemp() == 80 && insideTemp() < 62){
+            startAFire(fireplace1);
 
+            /*
+             * A one line answer i thought of was:
+             * if (outsideTemp() < 50 || insideTemp() < 62){
+             */ 
+
+        }
     }
+
 
     public void checkFuel(double fuelLevel) {
         // Write an IF statement that checks “fuelLevel” 
         // and if that check is less than 0.08, calls “refuel()”
+        if (fuelLevel < 0.08){
+            refuel();
 
+        }
     }
 
-
-    
     /**
      *  Pay no attention to the code below this point.
      * 
      * 
      * instance variables
-     * / 
-   int x;
-   int tt_t;
-   int tt_s;
-   int oo1, oo2;
-   String ss;
+     */
+    int x;
+    int tt_t;
+    int tt_s;
+    int oo1, oo2;
+    String ss;
 
-
-  /**
-   * Constructor for objects of class WriteIFs
-   */
-  public WriteIFs()
-  {
-      // initialise instance variables
-      x = 0;
-      tt_t = 0;
-      tt_s = 1;
-      ss = "";
-      oo1 = 61;
-      oo2 = 49;
-  }
+    /**
+     * Constructor for objects of class WriteIFs
+     */
+    public WriteIFs()
+    {
+        // initialise instance variables
+        x = 0;
+        tt_t = 0;
+        tt_s = 1;
+        ss = "";
+        oo1 = 61;
+        oo2 = 49;
+    }
 
     // associated routines
     public boolean isAlive(boolean p) {
         return !p;
     }
+
     private int tempurature(int t) {
         return t+2;
     }
+
     private void heatOn() {
         this.ss = "heating";
     }
+
     private void coolOn() {
         this.ss = "cooling";
     }
- 
+
     private int insideTemp() {
         return oo1;
     }
+
     private int outsideTemp() {
         return oo2;
     }
+
     private void startAFire(Object o) {
         this.tt_s = 213;
     }
+
     private void refuel() {
         this.x = 99;
     }
+
     private void displayGameOver(boolean b) {
         this.ss = "Game Over!";
     }
