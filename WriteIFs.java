1fb7b4bfe419c515155f884a8e8777987af98a8e
deltagphys <cty.jgates@gmail.com>
2019-10-18 16:36:57 -0400

    all tests passed

--- WriteIFs.java
+++ WriteIFs.java
@@ -11,6 +11,9 @@ public class WriteIFs
     public void playerDied(boolean player1) {
         // Write an IF statement that checks “player1.isAlive()” 
         // and if that’s false, calls “displayGameOver(player1)”
+        if (!isAlive(player1)) {
+            displayGameOver(player1);
+        }
      
     }
     
@@ -18,9 +21,11 @@ public class WriteIFs
         // Write an IF statement that checks the 
         // “temperature(room)” and if that check is less than 70, 
         // calls “heatOn()” else calls “coolOn()”
-
-
-        
+        if (temperature(room) < 70){
+            heatOn();
+        } else {
+            coolOn();
+        }
         return this.ss;
     }
 
@@ -30,13 +35,20 @@ public class WriteIFs
         // AND 
         // “insideTemp()” is less than 62, 
         // calls “startAFire(fireplace1)”
+        
+        if (outsideTemp() < 50 && insideTemp() < 62) {
+            startAFire(fireplace1);
+        } 
 
     }
 
     public void checkFuel(double fuelLevel) {
         // Write an IF statement that checks “fuelLevel” 
         // and if that check is less than 0.08, calls “refuel()”
-
+        
+        if (fuelLevel < .08) {
+            refuel();
+        }
     }
 
 
@@ -46,7 +58,8 @@ public class WriteIFs
      * 
      * 
      * instance variables
-     * / 
+     */ 
+   
    int x;
    int tt_t;
    int tt_s;
@@ -72,7 +85,7 @@ public class WriteIFs
     public boolean isAlive(boolean p) {
         return !p;
     }
-    private int tempurature(int t) {
+    private int temperature(int t) {
         return t+2;
     }
     private void heatOn() {
