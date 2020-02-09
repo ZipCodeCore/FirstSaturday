c1f1bf75454a8a609471c813de2c5a8d10a6baab
Sandeep Naraya <snaraya@zipcodes-MacBook-Pro-9.local>
2020-02-09 13:35:15 -0500

    Checkpointing my work

--- WriteIFs.java
+++ WriteIFs.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of class WriteIFs here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Sandeep Narayana Mangalam
+ * @version 02/09/2020
  */
 public class WriteIFs
 {
@@ -11,16 +11,31 @@ public class WriteIFs
     public void playerDied(boolean player1) {
         // Write an IF statement that checks “player1.isAlive()” 
         // and if that’s false, calls “displayGameOver(player1)”
-     
+        
+        if (isALive(true))
+        {
+            return true;
+    
+        }
+        else{
+            
+        displayGameOver(player1);
+    }
     }
     
     public String thermoSTAT(int room) {
         // Write an IF statement that checks the 
         // “temperature(room)” and if that check is less than 70, 
         // calls “heatOn()” else calls “coolOn()”
-
-
         
+        if(temperature(room)<70)
+        {
+            heatOn();
+        }
+        else {
+            coolOn();
+        }
+
         return this.ss;
     }
 
@@ -30,12 +45,21 @@ public class WriteIFs
         // AND 
         // “insideTemp()” is less than 62, 
         // calls “startAFire(fireplace1)”
+        
+        if((outsideTemp()<50) || (insideTemp()<62))
+        {
+            startAFire(fireplace1);
+        }
 
     }
 
     public void checkFuel(double fuelLevel) {
         // Write an IF statement that checks “fuelLevel” 
         // and if that check is less than 0.08, calls “refuel()”
+        
+        if (fuelLevel<0.08)
+        {
+            refuel();
 
     }
 
