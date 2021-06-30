b365992db405f2842d6a8c4ae405e9b061ba8781
jgiroso <jrgiroso@gmail.com>
2021-06-30 16:49:42 -0400

    made 1 additional change to checkServer loop

--- WriteLoops.java
+++ WriteLoops.java
@@ -218,7 +218,7 @@ public class WriteLoops {
         }
         if(serverIsRunning() == false) {
             sendEmergencyText("Help!", adminPhoneNumber);
-            
+            tryServerRestart("Help!", adminPhoneNumber);
         }
 
         // calling
