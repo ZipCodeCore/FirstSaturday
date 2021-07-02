33daa1c119fd84309431e0b618e621b5ff7e6d6c
TatianaDeAngelo <85692272+TatianaDeAngelo@users.noreply.github.com>
2021-07-01 20:49:17 -0400

    All WriteIftests are passing

--- WriteIFs.java
+++ WriteIFs.java
@@ -17,7 +17,7 @@ public class WriteIFs
     public void playerDied(boolean player1) {
           // Write an IF statement that checks “player1.isAlive()” 
         // and if that’s false, calls “displayGameOver(player1)”
-        if (isAlive(player1) !=true);{
+        if (isAlive(player1) !=true){
             displayGameOver(player1);
         }
             
@@ -47,12 +47,20 @@ public class WriteIFs
         // AND 
         // “insideTemp()” is less than 62, 
         // calls “startAFire(fireplace1)”
+        
+        
+        if (outsideTemp() < 50 && insideTemp() < 62) {
+            startAFire(fireplace1);
+        }
 
     }
 
     public void checkFuel(double fuelLevel) {
         // Write an IF statement that checks “fuelLevel” 
         // and if that check is less than 0.08, calls “refuel()”
+        if (fuelLevel < 0.08) {
+            refuel();
+        }
 
     }
 
